b4afb7f1c26a6576275f98c29dc6cca8b6254bc6
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-04-11 07:50:43 -0700

    5-to_integer.js fixed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const num = parseInt(process.argv[2]);
+const number = parseInt(process.argv[2], 10);
 
-if (!isNaN(num)){
-	console.log(`My number: $[num}`);
+if (isNaN(number)) {
+  console.log('Not a number');
 } else {
-	console.log("Not a number");
+  console.log('My number: ' + number);
 }
