9a41a532cbab225037fdddefc6c3024e961669b5
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-04-26 05:20:58 -0700

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script takes a letter as input, sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter, and handles
+the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    payload = {'q': q}
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data=payload)
+
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
