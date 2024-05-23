71b1b78517f5f35712cc57715d0a3f19f1fde3d5
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-05-22 18:03:05 -0700

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+function readFile(filePath) {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(data);
+        }
+    });
+}
+
+const filePath = process.argv[2];
+readFile(filePath);
+
